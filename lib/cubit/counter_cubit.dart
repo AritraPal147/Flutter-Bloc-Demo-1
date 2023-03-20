4ae0db21b45a0c1adb13d312d11ef93724c5630b
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-03-21 01:27:24 +0530

    added BlocListener and BlocConsumer

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -2,12 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0));
+  CounterCubit() : super(CounterState(counterValue: 0, wasIncremented: false));
 
   // state.counterValue gives the counterValue of current state.
   // increment() function emits a CounterState after incrementing counterValue by 1
-  void increment() => emit(CounterState(counterValue: state.counterValue+1));
+  void increment() => emit(CounterState(counterValue: state.counterValue+1, wasIncremented: true));
 
   // decrement() function emits a CounterState after decrementing counterValue by 1
-  void decrement() => emit(CounterState(counterValue: state.counterValue-1));
+  void decrement() => emit(CounterState(counterValue: state.counterValue-1, wasIncremented: false));
 }
